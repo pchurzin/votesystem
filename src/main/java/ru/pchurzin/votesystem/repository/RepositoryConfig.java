e1922396048ee14c39f92c8b7bb478397eb336bb
Pavel Churzin <pchurzin@pchurzin.ru>
2019-09-05 18:50:29 +0300

    adde @Configuration to config classes

--- src/main/java/ru/pchurzin/votesystem/repository/RepositoryConfig.java
+++ src/main/java/ru/pchurzin/votesystem/repository/RepositoryConfig.java
@@ -4,6 +4,7 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
@@ -11,6 +12,7 @@ import javax.sql.DataSource;
 
 @ComponentScan
 @PropertySource("classpath:db/db.properties")
+@Configuration
 public class RepositoryConfig {
 
     private final Environment env;
--- src/main/java/ru/pchurzin/votesystem/service/ServiceConfig.java
+++ src/main/java/ru/pchurzin/votesystem/service/ServiceConfig.java
@@ -1,7 +1,9 @@
 package ru.pchurzin.votesystem.service;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @ComponentScan
+@Configuration
 public class ServiceConfig {
 }
