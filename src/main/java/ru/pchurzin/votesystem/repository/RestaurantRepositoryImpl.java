8b1b06d6acc6d66c1fecb382e36eb273c8d582e2
Pavel Churzin <pchurzin@pchurzin.ru>
2019-09-05 17:13:00 +0300

    refactored restaurant repository dummy implementation

--- src/main/java/ru/pchurzin/votesystem/repository/RestaurantRepositoryImpl.java
+++ src/main/java/ru/pchurzin/votesystem/repository/RestaurantRepositoryImpl.java
@@ -4,26 +4,28 @@ import org.springframework.stereotype.Repository;
 import ru.pchurzin.votesystem.model.Restaurant;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
 
 @Repository
 public class RestaurantRepositoryImpl implements RestaurantRepository {
     @Override
-    public Restaurant save(Restaurant restaurant) {
-        return null;
+    public Optional<Restaurant> save(Restaurant restaurant) {
+        return Optional.empty();
     }
 
     @Override
-    public Restaurant findById(int id) {
-        return null;
+    public Optional<Restaurant> findById(int id) {
+        return Optional.empty();
     }
 
     @Override
     public Collection<Restaurant> findAll() {
-        return null;
+        return Collections.emptySet();
     }
 
     @Override
-    public void removeById(int id) {
-
+    public boolean removeById(int id) {
+        return false;
     }
 }
