866b8e3f0f6526b215a3dd0f6295d8edbbd9d728
Pavel Churzin <pchurzin@pchurzin.ru>
2019-09-05 16:47:39 +0300

    changed restaurant repository api

--- src/main/java/ru/pchurzin/votesystem/repository/RestaurantRepository.java
+++ src/main/java/ru/pchurzin/votesystem/repository/RestaurantRepository.java
@@ -3,14 +3,38 @@ package ru.pchurzin.votesystem.repository;
 import ru.pchurzin.votesystem.model.Restaurant;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public interface RestaurantRepository {
 
-    Restaurant save(Restaurant restaurant);
+    /**
+     * inserts or updates restaurant
+     *
+     * @param restaurant <code>Restaurant</code> to be inserted or updated
+     * @return <code>Optional</code> with updated restaurant or empty if operation didn't perform action
+     */
+    Optional<Restaurant> save(Restaurant restaurant);
 
-    Restaurant findById(int id);
+    /**
+     * fetch <code>Restaurant</code> by it's id
+     *
+     * @param id the id of the <code>Restaurant</code>
+     * @return Optional with found <code>Restaurant</code> or empty if no restaurant with id found
+     */
+    Optional<Restaurant> findById(int id);
 
+    /**
+     * fetch all restaurants
+     *
+     * @return <code>Collection</code> of <code>Restaurant</code>s or empty <code>Collection</code>
+     */
     Collection<Restaurant> findAll();
 
-    void removeById(int id);
+    /**
+     * deletes restaurant by id
+     *
+     * @param id the id of the <code>Restaurant</code> to be deleted
+     * @return if the restaurant has been removed
+     */
+    boolean removeById(int id);
 }
