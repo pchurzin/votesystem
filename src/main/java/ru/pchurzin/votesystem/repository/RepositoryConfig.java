b9f5a651f1c3afd3416bacdd191823aabc96626e
Pavel Churzin <pchurzin@pchurzin.ru>
2019-09-05 18:46:21 +0300

    added datasource

--- src/main/java/ru/pchurzin/votesystem/repository/RepositoryConfig.java
+++ src/main/java/ru/pchurzin/votesystem/repository/RepositoryConfig.java
@@ -1,8 +1,33 @@
 package ru.pchurzin.votesystem.repository;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
 
 @ComponentScan
+@PropertySource("classpath:db/db.properties")
 public class RepositoryConfig {
+
+    private final Environment env;
+
+    @Autowired
+    public RepositoryConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.user"));
+        dataSource.setPassword(env.getProperty("db.password"));
+        dataSource.setDriverClassName(env.getProperty("db.driverClass"));
+        return dataSource;
+    }
 
 }
