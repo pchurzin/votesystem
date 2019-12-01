122d57bd65788b11129ac08ddd1529078ed2655a
Pavel Churzin <pchurzin@pchurzin.ru>
2019-12-01 14:46:08 +0300

    refactored configuration

--- src/main/java/ru/pchurzin/votesystem/security/SecurityConfig.java
+++ src/main/java/ru/pchurzin/votesystem/security/SecurityConfig.java
@@ -1,14 +1,12 @@
 package ru.pchurzin.votesystem.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Configuration
-@ComponentScan
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
     @Bean
